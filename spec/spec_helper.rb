0fbe3a55d44bd66ad05d64412c8e11f0a3e94fbd
Toby Hsieh <thrasher13b@gmail.com>
2016-03-11 00:51:30 -0800

    Add GitHub login & logout to spec_helper.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,6 @@
+require 'selenium-webdriver'
+require 'yaml'
+
 RSpec.configure do |config|
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
@@ -16,4 +19,28 @@ RSpec.configure do |config|
   end
 
   config.order = :random
+
+  # selenium-webdriver is noisy
+  config.warnings = false
+
+  gh_config = YAML.load_file(File.join(__dir__, 'config.yml'))
+
+  config.before :context do
+    @driver = Selenium::WebDriver.for(
+      :chrome, args: ["--load-extension=#{File.join(__dir__, '..')}"]
+    )
+
+    @driver.get('https://github.com/login')
+    @driver.find_element(name: 'login').send_keys(gh_config['login'])
+    password_field = @driver.find_element(name: 'password')
+    password_field.send_keys(gh_config['password'])
+    password_field.submit
+  end
+
+  config.after :context do
+    @driver.find_element(css: '.header-nav-link .avatar').click
+    @driver.find_element(class: 'logout-form').click
+
+    @driver.quit
+  end
 end
