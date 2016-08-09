113fae925d93cffd18e071594ce5dfb8d9ab1760
Toby Hsieh <thrasher13b@gmail.com>
2016-08-08 22:03:05 -0700

    Use a closure in base.js

--- content_scripts/base.js
+++ content_scripts/base.js
@@ -1,21 +1,23 @@
-const EnhancedPullRequest = {
-  _callbacks: [],
+const EnhancedPullRequest = (function () {
+  const callbacks = [];
 
-  /**
-   * Registers the given callback to run on page load and pjax requests.
-   *
-   * @param {Function} callback - function to register
-   */
-  register_cb: function (callback) {
-    this._callbacks.push(callback);
-  },
+  return {
+    /**
+     * Registers the given callback to run on page load and pjax requests.
+     *
+     * @param {Function} callback - function to register
+     */
+    register_cb: function (callback) {
+      callbacks.push(callback);
+    },
 
-  /**
-   * Runs all callbacks.
-   */
-  run: function() {
-    // This isn't pretty, but I haven't found a better way yet.
-    $('.epr').remove();
-    this._callbacks.forEach(callback => callback());
-  }
-};
+    /**
+     * Runs all callbacks.
+     */
+    run: function () {
+      // This isn't pretty, but I haven't found a better way yet.
+      $('.epr').remove();
+      callbacks.forEach(callback => callback());
+    }
+  };
+})();
