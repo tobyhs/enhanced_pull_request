a409e533da6752462a295bec97655bccd35d8c8d
Toby Hsieh <thrasher13b@gmail.com>
2016-08-14 20:40:34 -0700

    Reorganize outdatedDiffComments.js

--- content_scripts/outdatedDiffComments.js
+++ content_scripts/outdatedDiffComments.js
@@ -1,9 +1,13 @@
-(function () {
-  EnhancedPullRequest.register_cb(function () {
-    // Button/link to show/hide comments on outdated diffs
+EnhancedPullRequest.outdatedDiffComments = {
+  /**
+   * Adds a button to toggle the visibility of comments on outdated diffs.
+   */
+  load: function () {
     $('<button type="button">Toggle Outdated Diff Comments</button>')
       .addClass('epr epr-toggle-outdated-comments btn-link')
       .click(() => $('.discussion-item-toggle-closed').click())
       .prependTo('.pull-discussion-timeline');
-  });
-})();
+  }
+}
+
+EnhancedPullRequest.register_cb(EnhancedPullRequest.outdatedDiffComments.load);
