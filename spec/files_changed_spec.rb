b459bd6e6a20f32abf1b2f220701358f8545cfd2
Toby Hsieh <thrasher13b@gmail.com>
2016-08-22 00:18:20 -0700

    Improve flakiness of one-file diff tests

--- spec/files_changed_spec.rb
+++ spec/files_changed_spec.rb
@@ -13,17 +13,26 @@ RSpec.describe 'Files changed page' do
 
   it_behaves_like 'a page with line comment replies'
 
-  context 'after clicking a one-file diff link' do
-    before do
+  it 'has one-file diff links' do
+    unless driver.find_element(:class, 'epr-one-file-diff').displayed?
       driver.find_element(:id, 'one-file-diff-btn').click
-      link = "#{pr_url}?eprFocus=sandbox/hello.rb"
-      driver.find_element(:css, %(a[href="#{link}"])).click
+    end
+    link = "#{pr_url}?eprFocus=sandbox/hello.rb"
+    expect(driver.find_element(:css, %(a[href="#{link}"]))).to be_displayed
+  end
+
+  context 'on a one-file diff page' do
+    let(:file) { 'sandbox/hello.rb' }
+
+    let(:pr_url) do
+      'https://github.com/tobyhs/enhanced_pull_request/pull/1/files' \
+        "?eprFocus=#{file}"
     end
 
     it 'has a title with the file name' do
       Selenium::WebDriver::Wait.new(
         message: 'Title does not contain the file name'
-      ).until { driver.title.start_with?('sandbox/hello.rb') }
+      ).until { driver.title.start_with?(file) }
     end
 
     it 'only shows the diff of the selected file' do
@@ -32,8 +41,9 @@ RSpec.describe 'Files changed page' do
           find_element(:css, '.file-header[data-path="sandbox/hello.py"]').
           displayed?
       end
+
       expect(
-        driver.find_element(:css, '.file-header[data-path="sandbox/hello.rb"]')
+        driver.find_element(:css, %(.file-header[data-path="#{file}"]))
       ).to be_displayed
     end
   end
