1da4c19858f45a4986a207d831d570d4d0337e7c
Toby Hsieh <thrasher13b@gmail.com>
2018-02-11 00:46:18 -0800

    Add comments to scripts involved in dealing with pjax

--- background.js
+++ background.js
@@ -1,3 +1,6 @@
+// GitHub uses pjax, so we can't solely rely on content scripts to run each
+// time. Instead, we listen for history changes and send messages to
+// content_scripts/main.js.
 chrome.webNavigation.onHistoryStateUpdated.addListener(
   function (details) {
     chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
--- content_scripts/main.js
+++ content_scripts/main.js
@@ -1,3 +1,6 @@
+// The following listener receives messages from the background script
+// (background.js) when the browser history changes and the URL is a GitHub
+// pull request URL.
 chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
   EnhancedPullRequest.run();
 });
